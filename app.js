e78aa0aea340d7376f987778bc77c476a405e679
Gust2491 <gulle8765@gmail.com>
2024-11-21 10:27:50 +0100

    Express V1

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.redirect('http://localhost:3000/views/employee-login.html');
+});
+
+app.get('/static/*', function (req, res) {
+    res.sendFile(path.join(__dirname, req.url));
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
