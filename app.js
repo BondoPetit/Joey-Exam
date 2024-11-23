b539df9e4aea34ee6ae59b1eed62156c6d6d49df
BondoPetit <166508819+BondoPetit@users.noreply.github.com>
2024-11-23 20:12:49 +0100

    lille fix

--- app.js
+++ app.js
@@ -1,20 +1,26 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors'); // Importer cors middleware
 const app = express();
 const PORT = 3000;
 
 // Import controllers for application functionalities
 const employee_login = require('./Static/controllers/employee_login');
-const { getPool } = require('./database'); 
-
+const { getPool } = require('./database');
 
 // Middleware setup
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Brug CORS middleware for at tillade anmodninger fra bestemte domæner
+app.use(cors({
+    origin: ['https://joe-and-the-juice.engineer'], // Tillad kun dette domæne at lave anmodninger
+    methods: ['GET', 'POST'], // Tillad kun GET og POST anmodninger
+    allowedHeaders: ['Content-Type', 'Authorization'], // Tillad specifikke headers
+}));
+
 // Serve static files
 app.use('/Static/public', express.static(path.join(__dirname, 'Static/public')));
-
 app.use('/views', express.static(path.join(__dirname, 'Static/views')));
 
 // Route to serve the start page at the root URL
